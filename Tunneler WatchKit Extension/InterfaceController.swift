d8882734bccd99006e9e60924f76c7a3b4a37702
Christian Di Lorenzo <rcddeveloper@icloud.com>
2017-05-06 19:45:13 -0400

    Initial Commit

---
+++ Tunneler WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Tunneler WatchKit Extension
+//
+//  Created by Christian Di Lorenzo on 5/6/17.
+//  Copyright © 2017 Christian Di Lorenzo. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
